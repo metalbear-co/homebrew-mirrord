b6d692abdd83de7569105118b44b51ba1ac29039
infiniteregrets <aroram18@mcmaster.ca>
2022-10-29 17:31:10 -0400

    Update to 3.4.0

--- mirrord.rb
+++ mirrord.rb
@@ -1,17 +1,16 @@
 class Mirrord < Formula
-    desc "Connect your local process and your cloud environment"
-    homepage "https://mirrord.dev"
-    url "https://github.com/metalbear-co/mirrord/releases/download/2.13.0/mirrord_mac_universal.zip"
-    sha256 "847f3eb90d46293f6f26f1e65eebdfb9712ea9b6e0be8438b72c8d7f20aa083b"
-    license "MIT"
-    head "https://github.com/metalbear-co/mirrord/releases/download/3.0.20-alpha/mirrord_mac_universal.zip"
-  
-    def install
-      bin.install "mirrord"
-    end
-  
-    test do
-      output = shell_output("mirrord --version")
-      assert_match "mirrord #{version}\n", output
-    end
+  desc "Connect your local process and your cloud environment"
+  homepage "https://mirrord.dev"
+  url "https://github.com/metalbear-co/mirrord/releases/download/3.4.0/mirrord_mac_universal.zip"
+  sha256 "4ad6657244b54272a00b1ea6c94e7b5683ddc42ed92157d6e1d42e3ae8450138"
+  license "MIT"
+
+  def install
+    bin.install "mirrord"
   end
+
+  test do
+    output = shell_output("mirrord --version")
+    assert_match "mirrord #{version}\n", output
+  end
+end
